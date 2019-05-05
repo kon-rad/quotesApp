45b3599bf49988100c0621acd693c8201587c72f
konradgnat <konradmgnat@gmail.com>
2019-05-04 21:34:59 -0500

    prog

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,19 @@ import React, { Component } from "react";
 
 export default class App extends Component {
   render() {
-    return <div>Hello React,Webpack 4 & Babel 7!</div>;
+    return (
+      <div className="container">
+        <div className="wrapper" id="quote-box">
+          <p id="text">
+            Quote here
+          </p>
+          <span id="author">
+            author here
+          </span>
+          <button id="new-quote">New Quote</button>
+          <button id="tweet-quote">Tweet Quote</button>
+        </div>
+      </div>
+    )
   }
 };
